daa30b45825603c152e6ca727dd3013b6ef1d338
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-07 15:56:51 +0100

    correzione img

--- js/script.js
+++ js/script.js
@@ -3,15 +3,21 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+
+// Salviamo in una variabile locale la funzione createApp, presa dalla variabile globale Vue.
 const { createApp } = Vue
 
+
+// Inizializziamo un'instanza dell'applicazione Vue, invocando la funzione createApp
 createApp ({
     data() {
 
         return{
             
             message: "Ciao sono un messaggio",
-            src: "img/spotify-lg.png"
+            src: "img/spotify-lg.png",
+            alt: "spotify"
         }
     }
+//     Invochiamo il metodo .mount() per connettere l'istanza al tag HTML con id #app e renderizzare l'app
 }).mount('#app')
