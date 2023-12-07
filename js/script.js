a288aa9bc579d84c89cfed001921282f64680c6f
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-07 15:41:26 +0100

    config: img

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue
+
+createApp ({
+    data() {
+
+        return{
+            
+            message: "Ciao sono un messaggio",
+            src: "img/spotify-lg.png"
+        }
+    }
+}).mount('#app')
